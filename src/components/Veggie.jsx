e86da82221c58849be419d521269fdd889f20de5
Samuel Jacobs <44222526+Samuel-jacobs@users.noreply.github.com>
2023-06-01 23:50:06 +0100

    new update

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
 
 function Veggie() {
+
+  const [ veggies, setVeggies ] = useState([]);
+
+  useEffect(() => {
+    getVeggies();
+  }, []);
+  const getVeggies = async () => {
+    const api = await fetch('')
+    const data = await api.json();
+
+    setVeggies(data.recipes);
+  }
   return (
     <div>Veggie</div>
   )
